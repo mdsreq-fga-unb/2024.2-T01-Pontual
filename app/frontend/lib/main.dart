f07edb02fa2e9b49a1f177f568d24df708360339
caiolamego <caio.lamego04@gmail.com>
2025-01-16 16:05:59 -0300

    cria estrutura da home screen

--- app/frontend/lib/main.dart
+++ app/frontend/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // Remove a bandeira "Debug"
       theme: appTheme,
       initialRoute: '/',
       routes: {
