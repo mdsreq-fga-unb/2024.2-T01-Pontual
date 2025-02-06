0a26b4c024675d80365dd53627dc0e9e0fdf8ff8
caiolamego <caio.lamego04@gmail.com>
2025-02-05 22:28:21 -0300

    cria página de configurações do admin

--- app/frontend/lib/screen/adminSettings_screen.dart
+++ app/frontend/lib/screen/adminSettings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/routes/app_routes.dart';
+import 'package:frontend/widgets/adminCards/settingsCard.dart';
 import 'package:frontend/widgets/adminUsebar.dart';
 import 'package:frontend/widgets/custom_appbar.dart';
 
@@ -9,10 +10,91 @@ class AdminSettingsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0XFFEDEDED),
       appBar: CustomAppBar(
-        title: "Configurações",
-        page: "Administrador",
+        title: "Administrador",
+        page: "Configurações",
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 16),
+            Text(
+              'Dados de Usuário',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Nome",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(
+                    "email",
+                    style:
+                        TextStyle(fontWeight: FontWeight.normal, fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Configurações de Perfil',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SettingsCard(
+                title: "Alterar Senha",
+                color: "#2DA5D0",
+                icon: Icon(
+                  Icons.edit,
+                  color: Color(0xFF2DA5D0),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Configurações de Admin',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SettingsCard(
+                title: "Adicionar Funcionário",
+                color: "#2DA5D0",
+                icon: Icon(
+                  Icons.person_add_alt,
+                  color: Color(0xFF2DA5D0),
+                )),
+            SettingsCard(
+                title: "Gerenciar Turmas",
+                color: "#2DA5D0",
+                icon: Icon(
+                  Icons.class_outlined,
+                  color: Color(0xFF2DA5D0),
+                )),
+            SettingsCard(
+                title: "Excluir Funcionário",
+                color: "#AB0D0D",
+                icon: Icon(
+                  Icons.delete_outlined,
+                  color: Color(0xFFFB0D0D),
+                )),
+          ],
+        ),
+      ),
       bottomNavigationBar: AdminUseBar(
         onHomePressed: () {
           Navigator.of(context).pushNamed(AppRoutes.AHP);
---
+++ app/frontend/lib/widgets/adminCards/settingsCard.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class SettingsCard extends StatelessWidget {
+  const SettingsCard(
+      {super.key,
+      required this.title,
+      required this.color,
+      required this.icon});
+
+  final String title;
+  final String color;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      height: 42,
+      width: 370,
+      decoration: BoxDecoration(
+        color: Color(0xFFD9D9D9),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "${title}",
+                  style: TextStyle(color: _parseColor(color), fontSize: 15),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: icon,
+                  color: Color(0xFF2DA5D0),
+                  iconSize: 25,
+                  padding: EdgeInsets.zero, // Remove padding extra
+                  constraints: BoxConstraints(), // Evita restrições extras
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color _parseColor(String hexColor) {
+    hexColor = hexColor.replaceAll("#", ""); // Remove o "#" se existir
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor"; // Adiciona a opacidade (FF = 100%)
+    }
+    return Color(int.parse(hexColor, radix: 16));
+  }
+}
