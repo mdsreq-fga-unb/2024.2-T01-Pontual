e0df40c58552d7208386660ab3ae756c2ff44740
mateusvrs <mateusvrs.dev@gmail.com>
2025-02-10 14:14:42 -0300

    feat: rotas do frontend

--- app/frontend/lib/routes/app_routes.dart
+++ app/frontend/lib/routes/app_routes.dart
@@ -3,4 +3,11 @@ class AppRoutes {
   static const Register = '/register';
   static const Profile = '/profile';
   static const Home = '/homepage';
+  static const Report = '/report';
+  static const AdminHome = '/admin-home';
+  static const AdminReport = '/admin-report';
+  static const AdminSettings = '/admin-settings';
+  static const AdminEmployeesReport = '/admin-employees-report';
+  static const AdminEmployeesSettings = '/admin-employees-settings';
+  static const AdminManageClasses = '/admin-manage-classes';
 }
